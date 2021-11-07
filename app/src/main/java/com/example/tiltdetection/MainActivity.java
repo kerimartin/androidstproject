fa80c4a56de96293663354eba9a31e1d61bf5991
kerimartin <keri.martin59@gmail.com>
2021-11-07 15:51:02 +0200

    First

---
+++ app/src/main/java/com/example/tiltdetection/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.tiltdetection;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
